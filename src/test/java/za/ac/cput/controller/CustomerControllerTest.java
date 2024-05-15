b962ed23a0ce18ed8162743232dbd297bb197a96
admin <gil.moreno998@gmail.com>
2024-05-15 14:30:51 +0200

    Controller

--- src/test/java/za/ac/cput/controller/CustomerControllerTest.java
+++ src/test/java/za/ac/cput/controller/CustomerControllerTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import za.ac.cput.domain.Customer;
 import za.ac.cput.factory.CustomerFactory;
@@ -39,13 +42,44 @@ class CustomerControllerTest {
 
     @Test
     void read() {
+        String url = BASE_URL + "/read" + customer.getCustomerId();
+        System.out.println("URL: "+ url);
+        ResponseEntity<Customer> response = restTemplate.getForEntity(url, Customer.class);
+        assertEquals(customer.getCustomerId(), response.getBody().getCustomerId());
+        System.out.println("Read: " + response.getBody());
     }
 
     @Test
     void update() {
+        String url = BASE_URL + "/update";
+        Customer newCustomer = new Customer.Builder().copy(customer)
+                .setFirstName("Brian").build();
+        ResponseEntity <Customer> postResponse = restTemplate.postForEntity(url, newCustomer, Customer.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+        //assertEquals(postResponse.getStatusCode().HttpStatus.OK);
+        Customer customerUpdated = postResponse.getBody();
+        assertEquals(newCustomer.getCustomerId(), customerUpdated.getCustomerId());
+        System.out.println("Updated data: " + customerUpdated);
     }
+
+//    @Test
+//    void delete (){
+//        String url = BASE_URL + "/delete/" + customer.getCustomerId();
+//        System.out.println("URL: "+url);
+//        restTemplate.delete(url);
+//        System.out.println("Success: Deleted customer");
+//    }
 
     @Test
     void getAll() {
+        String url = BASE_URL + "getAll";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity <String> entity = new HttpEntity<>(null, httpHeaders);
+        ResponseEntity <String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println("Show All: ");
+        System.out.println(response);
+        System.out.println(response.getBody());
+
     }
 }
